aab5ac81590ad8b730dacb9848d170964d6a2040
muhammedyasirk <84074691+muhammedyasirk@users.noreply.github.com>
2023-04-09 19:34:05 +0530

    Add files via upload

---
+++ Project.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.1;
+
+contract MyToken{
+
+    //public variable here
+    
+    string public token_name="MYBIT";
+    string public token_abbr="MYT";
+    uint public total_supply = 0;
+
+    //mapping variable here
+    mapping(address=> uint)public Balance;
+
+    //mint function
+    function mint(address sender,uint amount)public
+    {
+        Balance[sender]+=amount;
+        total_supply+=amount;
+    }
+    //burn function
+    function burn(address sender,uint amount)public
+    {
+        if(Balance[sender]>amount)
+        {
+            Balance[sender]-=amount;
+            total_supply-=amount;
+        }
+    }
+}
